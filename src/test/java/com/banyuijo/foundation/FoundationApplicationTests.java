e1b5a8906781d644c8af7aab1271dc560b3ee604
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-11 15:12:56 +0700

    comment

--- src/test/java/com/banyuijo/foundation/FoundationApplicationTests.java
+++ src/test/java/com/banyuijo/foundation/FoundationApplicationTests.java
@@ -1,13 +1,13 @@
-package com.banyuijo.foundation;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FoundationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.banyuijo.foundation;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FoundationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
