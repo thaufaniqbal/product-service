228f793b928b714f1880be2fc47104754b24e2c9
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-15 03:40:23 +0700

    site product service

--- src/main/java/com/banyuijo/foundation/config/GlobalExceptionHandler.java
+++ src/main/java/com/banyuijo/foundation/config/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.banyuijo.foundation.config;
 
 import com.banyuijo.foundation.dto.base.ApiResponseDto;
 import com.banyuijo.foundation.exception.HttpStatusException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,11 +15,13 @@ import java.util.HashMap;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private final ObjectMapper objectMapper;
     @ExceptionHandler(HttpStatusException.class)
-    public final ResponseEntity<ApiResponseDto> error(HttpStatusException ex) {
+    public final ResponseEntity<ApiResponseDto> error(HttpStatusException ex) throws JsonProcessingException {
         ApiResponseDto response = new ApiResponseDto(ex.getError().getStatus(), new HashMap<>(), ex.getError().getFormat());
-        log.error(response.toString());
+        log.error(objectMapper.writeValueAsString(response));
         return new ResponseEntity<>(response, ex.getError().getStatus());
     }
 
