0755703e21f8fa47090524a98205a5c9d5a6ba03
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-15 02:47:02 +0700

    site product service

--- src/main/java/com/banyuijo/foundation/controller/SiteProductController.java
+++ src/main/java/com/banyuijo/foundation/controller/SiteProductController.java
@@ -2,8 +2,10 @@ package com.banyuijo.foundation.controller;
 
 import com.banyuijo.foundation.dto.base.ApiResponseDto;
 import com.banyuijo.foundation.dto.product.site.SiteProductCreateInput;
+import com.banyuijo.foundation.dto.product.site.SiteProductEditInput;
 import com.banyuijo.foundation.service.product.site.SiteProductService;
 import com.banyuijo.foundation.service.product.site.create.SiteProductCreateService;
+import com.banyuijo.foundation.service.product.site.edit.SiteProductEditService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +19,20 @@ import java.util.UUID;
 public class SiteProductController {
     private final SiteProductService siteProductService;
     private final SiteProductCreateService siteProductCreateService;
-    @GetMapping("/{productId}")
-    public ResponseEntity<ApiResponseDto<Object>> getDetail(@PathVariable UUID productId) {
-        return ApiResponseDto.toResponseEntity(HttpStatus.OK, siteProductService.getProductDetail(productId));
+    private final SiteProductEditService siteProductEditService;
+    @GetMapping("/{siteProductId}")
+    public ResponseEntity<ApiResponseDto<Object>> getDetail(@PathVariable UUID siteProductId) {
+        return ApiResponseDto.toResponseEntity(HttpStatus.OK, siteProductService.getProductDetail(siteProductId));
     }
-    @GetMapping("/create")
-    public ResponseEntity<ApiResponseDto<Object>> getDetail(@RequestBody SiteProductCreateInput request,
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponseDto<Object>> createProduct(@RequestBody SiteProductCreateInput request,
                                                             @RequestHeader("login-id") String loginId) {
         return ApiResponseDto.toResponseEntity(HttpStatus.CREATED, siteProductCreateService.createProduct(request, loginId));
+    }
+    @PutMapping("/{siteProductId}")
+    public ResponseEntity<ApiResponseDto<Object>> editProduct(@RequestBody SiteProductEditInput request,
+                                                                @RequestHeader("login-id") String loginId,
+                                                                @PathVariable UUID siteProductId) {
+        return ApiResponseDto.toResponseEntity(HttpStatus.ACCEPTED, siteProductEditService.editProduct(request, loginId, siteProductId));
     }
 }
