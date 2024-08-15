9866891806cfa7557eea2b0aaa4fdc54edf40be1
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-15 14:47:55 +0700

    product type controller and service

--- src/main/java/com/banyuijo/foundation/dto/product/site/structure/detail/SiteProductDetailStructureOutput.java
+++ src/main/java/com/banyuijo/foundation/dto/product/site/structure/detail/SiteProductDetailStructureOutput.java
@@ -14,4 +14,5 @@ import java.util.UUID;
 public class SiteProductDetailStructureOutput {
     private UUID siteProductId;
     private HashMap<Integer, String> structure;
+    private String jsonStructure;
 }
