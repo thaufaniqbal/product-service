3349ef6200e5bc1d86769b2dd9e1a44ad3ed2809
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-15 13:11:38 +0700

    handler

--- src/main/java/com/banyuijo/foundation/service/product/type/ProductTypeServiceImpl.java
+++ src/main/java/com/banyuijo/foundation/service/product/type/ProductTypeServiceImpl.java
@@ -2,8 +2,6 @@ package com.banyuijo.foundation.service.product.type;
 
 import com.banyuijo.foundation.dto.product.type.ProductCodeListOutput;
 import com.banyuijo.foundation.entity.ProductType;
-import com.banyuijo.foundation.enums.HttpStatusCode;
-import com.banyuijo.foundation.exception.HttpStatusException;
 import com.banyuijo.foundation.repository.ProductTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,9 +16,6 @@ public class ProductTypeServiceImpl implements ProductTypeService {
     @Override
     public List<ProductCodeListOutput> getAllProductCode() {
         List<ProductType> productTypes = productTypeRepository.findAll();
-        if (productTypes.isEmpty()){
-            throw new HttpStatusException(HttpStatusCode.DATA_NOT_FOUND);
-        }
         return productTypes.stream()
                 .map(pt -> new ProductCodeListOutput(pt.getProductTypeId(), pt.getProductTypeCode()))
                 .collect(Collectors.toList());
