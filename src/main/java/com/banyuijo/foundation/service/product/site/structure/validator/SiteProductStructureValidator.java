4d260abb618846552d0bcabe8e2dcd761b90e0ab
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-15 14:42:37 +0700

    product type controller and service

---
+++ src/main/java/com/banyuijo/foundation/service/product/site/structure/validator/SiteProductStructureValidator.java
@@ -0,0 +1,11 @@
+package com.banyuijo.foundation.service.product.site.structure.validator;
+
+import com.banyuijo.foundation.validator.GlobalValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SiteProductStructureValidator extends GlobalValidator {
+
+}
