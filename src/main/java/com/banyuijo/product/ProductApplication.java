9fabef5a6eb83deb216a6a93e27e616dcfc80e26
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-17 02:15:30 +0700

    change directory name

--- src/main/java/com/banyuijo/product/ProductApplication.java
+++ src/main/java/com/banyuijo/product/ProductApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoundationApplication {
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoundationApplication.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
