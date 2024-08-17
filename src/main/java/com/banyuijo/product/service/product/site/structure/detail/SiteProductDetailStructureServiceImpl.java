0e17919f43b9188f39e146481cbf01b9d4932f26
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-18 03:33:05 +0700

    add site product detail

--- src/main/java/com/banyuijo/product/service/product/site/structure/detail/SiteProductDetailStructureServiceImpl.java
+++ src/main/java/com/banyuijo/product/service/product/site/structure/detail/SiteProductDetailStructureServiceImpl.java
@@ -28,7 +28,7 @@ public class SiteProductDetailStructureServiceImpl implements SiteProductDetailS
 
         List<SiteProductStructure.SiteBaseProductStructure> siteBaseProductStructures = buildStructure(siteProductId);
 
-        output.setSiteBaseProductParentName("hidroponik");
+        output.setSiteBaseProductParentName("hidroponik dummy");
         output.setStructures(siteBaseProductStructures);
 
         return output;
@@ -38,7 +38,7 @@ public class SiteProductDetailStructureServiceImpl implements SiteProductDetailS
 
         SiteProductStructure.SiteBaseProductStructure structure1 = new SiteProductStructure.SiteBaseProductStructure();
         structure1.setSeq(1);
-        structure1.setSiteBaseProductStructureName("Structure 1");
+        structure1.setSiteBaseProductStructureName("pH");
 
         SiteProductStructure.SiteBaseProductStructure.SiteBaseProductSettingData setting1 = new SiteProductStructure.SiteBaseProductStructure.SiteBaseProductSettingData();
         setting1.setSeq(1);
@@ -48,19 +48,19 @@ public class SiteProductDetailStructureServiceImpl implements SiteProductDetailS
 
         SiteProductStructure.SiteBaseProductStructure structure2 = new SiteProductStructure.SiteBaseProductStructure();
         structure2.setSeq(2);
-        structure2.setSiteBaseProductStructureName("Structure 2");
+        structure2.setSiteBaseProductStructureName("alarm");
 
         SiteProductStructure.SiteBaseProductStructure.SiteBaseProductSettingData setting3 = new SiteProductStructure.SiteBaseProductStructure.SiteBaseProductSettingData();
         setting3.setSeq(1);
-        setting3.setValue("Value 2");
-        setting3.setUpperBond("Upper Bond 1");
-        setting3.setLowerBond("Lower Bond 2");
+        setting3.setValue("ke-1");
+        setting3.setUpperBond("07:00");
+        setting3.setLowerBond("12:00");
 
         SiteProductStructure.SiteBaseProductStructure.SiteBaseProductSettingData setting4 = new SiteProductStructure.SiteBaseProductStructure.SiteBaseProductSettingData();
         setting4.setSeq(2);
-        setting4.setUpperBond("Upper Bond 3");
-        setting4.setLowerBond("Lower Bond 4");
-
+        setting3.setValue("ke-2");
+        setting4.setUpperBond("15:00");
+        setting4.setLowerBond("21:00");
         structure2.setSettings(List.of(setting3, setting4));
 
         return List.of(structure1, structure2);
