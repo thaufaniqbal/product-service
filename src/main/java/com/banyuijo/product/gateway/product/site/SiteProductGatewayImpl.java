a3683a566b80595c5196a48b31cf3ab60c5d1d07
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-21 20:06:14 +0700

    change for move service

--- src/main/java/com/banyuijo/product/gateway/product/site/SiteProductGatewayImpl.java
+++ src/main/java/com/banyuijo/product/gateway/product/site/SiteProductGatewayImpl.java
@@ -2,23 +2,81 @@ package com.banyuijo.product.gateway.product.site;
 
 import com.banyuijo.product.dto.product.site.SiteProductListOutput;
 import com.banyuijo.product.dto.product.site.SiteProductSearchOutput;
+import com.banyuijo.product.entity.QProductType;
+import com.banyuijo.product.entity.QSiteProduct;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.QBean;
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 @Component
 @RequiredArgsConstructor
 public class SiteProductGatewayImpl implements SiteProductGateway {
+    private final EntityManager entityManager;
     @Override
     public List<SiteProductListOutput> getListByDeleteStatusAndProductTypeId(int deleteStatus, UUID productTypeId) {
-        return null;
+        JPAQueryFactory queryFactory = new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+        QProductType productType = QProductType.productType;
+        QSiteProduct siteProduct = QSiteProduct.siteProduct;
+
+        QBean<SiteProductListOutput> entity = Projections.fields(SiteProductListOutput.class,
+                siteProduct.siteProductId,
+                siteProduct.siteProductName,
+                siteProduct.siteProductCode
+        );
+        JPAQuery<SiteProductListOutput> query = queryFactory.
+                select(entity).
+                from(siteProduct).
+                join(productType).
+                on(siteProduct.productTypeId.eq(productType.productTypeId)).
+                orderBy(siteProduct.siteProductCode.asc());
+        if (Objects.nonNull(productTypeId)){
+            query.where(productType.productTypeId.eq(productTypeId));
+        }
+        return query.fetch();
     }
 
     @Override
     public Page<SiteProductSearchOutput> getSearchSiteProduct(String productCode, String productName, UUID productTypeId, Pageable pageable) {
-        return null;
+        JPAQueryFactory queryFactory = new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+        QProductType productType = QProductType.productType;
+        QSiteProduct siteProduct = QSiteProduct.siteProduct;
+
+        QBean<SiteProductSearchOutput> entity = Projections.fields(SiteProductSearchOutput.class,
+                siteProduct.siteProductId,
+                siteProduct.siteProductName,
+                siteProduct.siteProductCode,
+                productType.productTypeId,
+                productType.productTypeCode,
+                siteProduct.lastUpdatedDate
+        );
+        JPAQuery<SiteProductSearchOutput> query = queryFactory.
+                select(entity).
+                from(siteProduct).
+                join(productType).
+                on(siteProduct.productTypeId.eq(productType.productTypeId)).
+                where(
+                        siteProduct.siteProductName.likeIgnoreCase(productName),
+                        siteProduct.siteProductCode.likeIgnoreCase(productCode)
+                        ).
+                limit(pageable.getPageSize()).
+                offset(pageable.getOffset()).
+                orderBy(siteProduct.siteProductCode.asc());
+        if (Objects.nonNull(productTypeId)){
+            query.where(productType.productTypeId.eq(productTypeId));
+        }
+        List<SiteProductSearchOutput> result = query.fetch();
+        long totalCount = query.fetchCount();
+        return new PageImpl<>(result, pageable, totalCount);
     }
 }
--- src/main/java/com/banyuijo/product/gateway/product/type/ProductTypeGatewayImpl.java
+++ src/main/java/com/banyuijo/product/gateway/product/type/ProductTypeGatewayImpl.java
@@ -2,22 +2,66 @@ package com.banyuijo.product.gateway.product.type;
 
 import com.banyuijo.product.dto.product.type.ProductTypeListOutput;
 import com.banyuijo.product.dto.product.type.ProductTypeSearchOutput;
+import com.banyuijo.product.entity.QProductType;
+import com.banyuijo.product.enums.BooleanStatus;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.QBean;
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class ProductTypeGatewayImpl implements ProductTypeGateway {
+    private final EntityManager entityManager;
     @Override
     public List<ProductTypeListOutput> findAllByDeleteStatus(int deleteStatus) {
-        return null;
+        JPAQueryFactory queryFactory = new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+        QProductType productType = QProductType.productType;
+
+        QBean<ProductTypeListOutput> entity = Projections.fields(ProductTypeListOutput.class,
+                productType.productTypeId,
+                productType.productTypeCode
+        );
+        JPAQuery<ProductTypeListOutput> query = queryFactory.
+                select(entity).
+                from(productType).
+                where(productType.deleteStatus.eq(deleteStatus)).
+                orderBy(productType.productTypeCode.asc());
+        return query.fetch();
     }
 
     @Override
     public Page<ProductTypeSearchOutput> getSearchSiteProduct(String productCode, String productName, Pageable pageable) {
-        return null;
+        JPAQueryFactory queryFactory = new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+        QProductType productType = QProductType.productType;
+
+        QBean<ProductTypeSearchOutput> entity = Projections.fields(ProductTypeSearchOutput.class,
+                productType.productTypeId,
+                productType.productTypeCode,
+                productType.lastUpdatedDate
+        );
+        JPAQuery<ProductTypeSearchOutput> query = queryFactory.
+                select(entity).
+                from(productType).
+                where(
+                        productType.productTypeName.likeIgnoreCase(productName),
+                        productType.productTypeCode.likeIgnoreCase(productCode),
+                        productType.deleteStatus.eq(BooleanStatus.NO.getCode())
+                ).
+                limit(pageable.getPageSize()).
+                offset(pageable.getOffset()).
+                orderBy(productType.productTypeCode.asc());
+        List<ProductTypeSearchOutput> result = query.fetch();
+        long totalCount = query.fetchCount();
+        return new PageImpl<>(result, pageable, totalCount);
     }
 }
