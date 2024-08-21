eef8a84f1af2e3adeca1b9aab289acb24d67cb40
thaufaniqbal <t.iqbal.a@gmail.com>
2024-08-22 01:30:41 +0700

    change for move service

--- src/main/java/com/banyuijo/product/gateway/product/type/ProductTypeGatewayImpl.java
+++ src/main/java/com/banyuijo/product/gateway/product/type/ProductTypeGatewayImpl.java
@@ -53,8 +53,8 @@ public class ProductTypeGatewayImpl implements ProductTypeGateway {
                 select(entity).
                 from(productType).
                 where(
-                        productType.productTypeName.likeIgnoreCase(productName),
-                        productType.productTypeCode.likeIgnoreCase(productCode),
+                        productType.productTypeName.equalsIgnoreCase(productName),
+                        productType.productTypeCode.equalsIgnoreCase(productCode),
                         productType.deleteStatus.eq(BooleanStatus.NO.getCode())
                 ).
                 limit(pageable.getPageSize()).
