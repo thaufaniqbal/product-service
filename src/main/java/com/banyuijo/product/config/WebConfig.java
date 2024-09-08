5fba22f7bea986a1a694d0f0d209cf6eca15eac0
thaufaniqbal <t.iqbal.a@gmail.com>
2024-09-08 21:49:24 +0700

    fix structure detail

--- src/main/java/com/banyuijo/product/config/WebConfig.java
+++ src/main/java/com/banyuijo/product/config/WebConfig.java
@@ -9,11 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Apply CORS to all endpoints
-                .allowedOrigins("*")  // Allow all origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow necessary HTTP methods
-                .allowedHeaders("*")  // Allow all headers
-                .allowCredentials(true)  // Allow credentials such as cookies
-                .maxAge(3600);  // Cache the CORS configuration for 1 hour
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")  // Menggunakan allowedOriginPatterns alih-alih allowedOrigins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)  // Mengizinkan kredensial seperti cookie atau header Authorization
+                .maxAge(3600);
     }
 }
