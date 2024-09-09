c58254bc20d968af03589610dd661e7a5676e08e
thaufaniqbal <t.iqbal.a@gmail.com>
2024-09-09 19:09:22 +0700

    fix delete status

--- src/main/java/com/banyuijo/product/service/product/site/delete/SiteProductDeleteServiceImpl.java
+++ src/main/java/com/banyuijo/product/service/product/site/delete/SiteProductDeleteServiceImpl.java
@@ -4,6 +4,7 @@ import com.banyuijo.product.entity.SiteProduct;
 import com.banyuijo.product.enums.BooleanStatus;
 import com.banyuijo.product.repository.SiteProductRepository;
 import com.banyuijo.product.service.product.site.validator.SiteProductValidator;
+import com.banyuijo.product.util.CustomLogger;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,13 @@ import java.util.UUID;
 public class SiteProductDeleteServiceImpl implements SiteProductDeleteService {
     private final SiteProductRepository siteProductRepository;
     private final SiteProductValidator validator;
+    private final CustomLogger customLogger;
     @Override
     public Object deleteSiteProduct(UUID siteProductId, String loginId) throws JsonProcessingException {
         validator.validateSiteProductId(siteProductId);
         SiteProduct siteProduct = siteProductRepository.findBySiteProductId(siteProductId);
         siteProduct.setDeleteStatus(BooleanStatus.YES.getCode());
+        customLogger.setLogObject(siteProduct, "deleteSiteProduct", loginId);
         siteProductRepository.save(siteProduct);
         return siteProduct;
     }
