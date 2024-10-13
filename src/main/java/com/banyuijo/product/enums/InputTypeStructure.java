ba0712741a05e31e06d859b7f0160e35f01af71d
thaufaniqbal <t.iqbal.a@gmail.com>
2024-10-13 22:53:57 +0700

    detail structure & edit

--- src/main/java/com/banyuijo/product/enums/InputTypeStructure.java
+++ src/main/java/com/banyuijo/product/enums/InputTypeStructure.java
@@ -12,13 +12,13 @@ public enum InputTypeStructure {
     INPUT_TYPE1 (1, "%s", "%s", "%s", "input, input, input"),
     INPUT_TYPE2 (2, "%s", "%s", "%s", "input, display, display"),
     INPUT_TYPE3 (3, "%s", "%s", "%s", "display, input, input"),
-    INPUT_TYPE4 (3, "%s", "%s", "%s", "display, display, display"),
-    INPUT_TYPE5 (4, "%s", null, null, "input, null, null"),
-    INPUT_TYPE6 (5, "%s", null, null, "display, null, null"),
-    INPUT_TYPE7 (6, null, "%s", "%s", "null, input, input"),
-    INPUT_TYPE8 (7, null, "%s", "%s", "null, display, display"),
-    INPUT_TYPE9 (8, null, "%s", "%s", "null, input, display"),
-    INPUT_TYPE10 (9, null, "%s", "%s", "null, display, input");
+    INPUT_TYPE4 (4, "%s", "%s", "%s", "display, display, display"),
+    INPUT_TYPE5 (5, "%s", null, null, "input, null, null"),
+    INPUT_TYPE6 (6, "%s", null, null, "display, null, null"),
+    INPUT_TYPE7 (7, null, "%s", "%s", "null, input, input"),
+    INPUT_TYPE8 (8, null, "%s", "%s", "null, display, display"),
+    INPUT_TYPE9 (9, null, "%s", "%s", "null, input, display"),
+    INPUT_TYPE10 (10, null, "%s", "%s", "null, display, input");
 
     private final int code;
     private final String value;
