ff768f34f2a615a4f5ed915c5f3e075e996f2e94
thaufaniqbal <t.iqbal.a@gmail.com>
2025-02-11 21:19:31 +0700

    rebuild to monolith, setup service

--- src/main/java/com/iconnect/product/config/WebConfig.java
+++ src/main/java/com/iconnect/product/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://banyu-ijo-4l8e.vercel.app", "http://192.168.100.51:5173/") // Tambahkan origin yang diperbolehkan
+                .allowedOriginPatterns("https://banyu-ijo-4l8e.vercel.app", "http://192.168.100.51:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("'Access-Control-Allow-Origin'")
+                .exposedHeaders("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Cache-Control")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
