ee70ab96378f1e52b4a7a614f4fe9e6f35e34ddd
thaufaniqbal <t.iqbal.a@gmail.com>
2025-02-11 22:25:39 +0700

    rebuild to monolith, setup service

--- src/main/java/com/iconnect/product/config/SimpleCORSFilter.java
+++ src/main/java/com/iconnect/product/config/SimpleCORSFilter.java
@@ -18,7 +18,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, user-id, login-id");
         response.setHeader("Access-Control-Expose-Headers", "Authorization");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
