42208d33d902ff9deb1bc23ea21a590183a187ac
thaufaniqbal <t.iqbal.a@gmail.com>
2025-02-11 21:05:20 +0700

    rebuild to monolith, setup service

--- src/main/java/com/iconnect/product/config/WebConfig.java
+++ src/main/java/com/iconnect/product/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
