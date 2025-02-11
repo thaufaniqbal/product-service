d4fc267dd9a8939f0d8bc4a889cbbc27be5adef2
thaufaniqbal <t.iqbal.a@gmail.com>
2025-02-11 21:50:53 +0700

    rebuild to monolith, setup service

--- src/main/java/com/iconnect/product/config/SimpleCORSFilter.java
+++ src/main/java/com/iconnect/product/config/SimpleCORSFilter.java
@@ -5,31 +5,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
-import java.util.List;
 
 @Component
 public class SimpleCORSFilter implements Filter {
-
-    private static final List<String> ALLOWED_ORIGINS = List.of(
-            "https://banyu-ijo-4l8e.vercel.app",
-            "http://192.168.100.51:5173"
-    );
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        String origin = request.getHeader("Origin");
-//
-//        if (ALLOWED_ORIGINS.contains(origin)) {
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-            response.setHeader("Access-Control-Expose-Headers", "Authorization");
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-//        }
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
