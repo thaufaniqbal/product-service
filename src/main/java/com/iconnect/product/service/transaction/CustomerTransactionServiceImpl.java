d79c2edc5054ac71521085ed4f73796dbde23a4b
thaufaniqbal <t.iqbal.a@gmail.com>
2025-02-15 16:13:28 +0700

    monolith transaction

--- src/main/java/com/iconnect/product/service/transaction/CustomerTransactionServiceImpl.java
+++ src/main/java/com/iconnect/product/service/transaction/CustomerTransactionServiceImpl.java
@@ -17,7 +17,6 @@ import com.iconnect.product.repository.transaction.customer.CustomerTransactionR
 import com.iconnect.product.service.integration.validator.IntegrationUtil;
 import com.iconnect.product.service.product.product.site.template.detail.SiteProductTemplateDetailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -60,8 +59,7 @@ public class CustomerTransactionServiceImpl implements CustomerTransactionServic
         if (Objects.isNull(transactionMapping)){
             return result;
         }
-//        setTransactionData(result, templateOutput, transactionMapping);
-        return result;
+        return setTransactionData(result, templateOutput, transactionMapping);
     }
 
     @Override
@@ -104,15 +102,109 @@ public class CustomerTransactionServiceImpl implements CustomerTransactionServic
         customerTransactionRepository.save(data);
         return data;
     }
-    private void setTransactionData (CustomerTransactionDataOutput result,
-                                     SiteProductTemplateOutput templateOutput,
-                                     CustomerTransactionMapping transactionMapping) throws IOException {
-        CustomerTransaction data = customerTransactionRepository.
-                findTopByCustomerTransactionMappingIdOrderByCreatedDateDesc(transactionMapping.getCustomerTransactionMappingId());
-        if (Objects.nonNull(data)){
-            CustomerTransactionDataOutput transactionDataOutput = mapper.readValue(data.getData(), CustomerTransactionDataOutput.class);
-            BeanUtils.copyProperties(transactionDataOutput, result);
+    private CustomerTransactionDataOutput setTransactionData(
+            CustomerTransactionDataOutput result,
+            SiteProductTemplateOutput templateOutput,
+            CustomerTransactionMapping transactionMapping) throws IOException {
+
+        CustomerTransaction data = customerTransactionRepository
+                .findTopByCustomerTransactionMappingIdOrderByCreatedDateDesc(
+                        transactionMapping.getCustomerTransactionMappingId());
+
+        if (Objects.isNull(data)) {
+            return result;
         }
+
+        CustomerTransactionDataOutput newResult = new CustomerTransactionDataOutput();
+        CustomerTransactionDataOutput transactionDataOutput = mapper.readValue(
+                data.getData(),
+                CustomerTransactionDataOutput.class
+        );
+
+        newResult.setSiteProductId(templateOutput.getSiteProductId());
+        newResult.setStructures(processStructures(templateOutput, transactionDataOutput));
+
+        return mapper.convertValue(newResult, CustomerTransactionDataOutput.class);
+    }
+
+    private List<CustomerTransactionDataOutput.StructureDTO> processStructures(
+            SiteProductTemplateOutput templateOutput,
+            CustomerTransactionDataOutput transactionDataOutput) {
+
+        List<CustomerTransactionDataOutput.StructureDTO> newStructures = new ArrayList<>();
+
+        templateOutput.getStructures().forEach(structureTemplate -> {
+            CustomerTransactionDataOutput.StructureDTO newStructure = createStructure(structureTemplate);
+            newStructure.setCardTemplate(
+                    processCardTemplates(structureTemplate, transactionDataOutput)
+            );
+            newStructures.add(newStructure);
+        });
+
+        return newStructures;
+    }
+
+    private CustomerTransactionDataOutput.StructureDTO createStructure(
+            SiteProductTemplateOutput.StructureDTO structureTemplate) {
+
+        CustomerTransactionDataOutput.StructureDTO newStructure =
+                new CustomerTransactionDataOutput.StructureDTO();
+        newStructure.setSeq(structureTemplate.getSeq());
+        newStructure.setSiteBaseProductStructureName(
+                structureTemplate.getSiteBaseProductStructureName()
+        );
+        return newStructure;
+    }
+
+    private List<CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO> processCardTemplates(
+            SiteProductTemplateOutput.StructureDTO structureTemplate,
+            CustomerTransactionDataOutput transactionDataOutput) {
+
+        List<CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO> newCardTemplates =
+                new ArrayList<>();
+
+        structureTemplate.getCardTemplate().forEach(structureCardTemplate -> {
+            CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO newCardTemplate =
+                    findAndMapCardTemplate(structureCardTemplate, transactionDataOutput);
+            newCardTemplates.add(newCardTemplate);
+        });
+
+        return newCardTemplates;
+    }
+
+    private CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO findAndMapCardTemplate(
+            SiteProductTemplateOutput.StructureDTO.CardTemplateDTO structureCardTemplate,
+            CustomerTransactionDataOutput transactionDataOutput) {
+
+        CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO newCardTemplate =
+                new CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO();
+
+        transactionDataOutput.getStructures().forEach(structure ->
+                structure.getCardTemplate().forEach(cardTemplate -> {
+                    if (cardTemplate.getSettingCode()
+                            .equalsIgnoreCase(structureCardTemplate.getSettingCode())) {
+                        mapCardTemplateData(newCardTemplate, cardTemplate);
+                    }
+                })
+        );
+
+        return newCardTemplate;
+    }
+
+    private void mapCardTemplateData(
+            CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO target,
+            CustomerTransactionDataOutput.StructureDTO.CardTemplateDTO source) {
+
+        target.setSeq(source.getSeq());
+        target.setSettingCode(source.getSettingCode());
+        target.setComponentValue(source.getComponentValue());
+        target.setValue(source.getValue());
+        target.setLowerBond(source.getLowerBond());
+        target.setUpperBond(source.getUpperBond());
+
+        SiteBaseProductSettingData settingData = settingDataRepository
+                .findBySettingCodeIgnoreCase(source.getSettingCode());
+        target.setDescription(settingData.getObjectName());
     }
 }
 
