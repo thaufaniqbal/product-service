941c4506f94949f44a949d47c4f337378fa58b80
thaufaniqbal <t.iqbal.a@gmail.com>
2026-02-02 00:08:14 +0700

    monolith transaction

--- src/main/java/com/iconnect/product/controller/transaction/DeviceTransactionController.java
+++ src/main/java/com/iconnect/product/controller/transaction/DeviceTransactionController.java
@@ -1,6 +1,8 @@
 package com.iconnect.product.controller.transaction;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.iconnect.product.dto.ApiResponseDTO;
+import com.iconnect.product.dto.transaction.DeviceTransactionInputOutput;
 import com.iconnect.product.service.transaction.device.DeviceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,9 +17,16 @@ import java.util.UUID;
 @RequestMapping("/device/v1/transaction")
 public class DeviceTransactionController {
     private final DeviceService deviceService;
+
     @GetMapping("/get-data/{siteProductId}")
     public ResponseEntity<ApiResponseDTO<Object>> getData(@PathVariable UUID siteProductId,
                                                           @RequestHeader("user-id") UUID userId) throws IOException {
         return ApiResponseDTO.toResponseEntity(HttpStatus.OK, deviceService.getData(userId, siteProductId));
+    }
+
+    @PostMapping("/save-data")
+    public ResponseEntity<ApiResponseDTO<Object>> saveData(@RequestBody DeviceTransactionInputOutput input,
+                                                           @RequestHeader("user-id") UUID userId) throws JsonProcessingException {
+        return ApiResponseDTO.toResponseEntity(HttpStatus.CREATED, deviceService.saveData(userId, input));
     }
 }
